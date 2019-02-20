79e9ba49abf9a0a0cbc6e3ce64bd9fca86e8662c
ruanhuasi <280128121@qq.com>
2019-02-20 23:19:07 +0800

    init my poject

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- 轮播图区域 -->
+    <mt-swipe :auto="4000">
+      <!-- 在组件中，使用v-for循环的话，一定要使用 key -->
+      <!-- <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
+        <img :src="item.img" alt="">
+      </mt-swipe-item>-->
+      <mt-swipe-item>
+        
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
+</template>
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
